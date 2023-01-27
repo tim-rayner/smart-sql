d145d7702f86718efb412ca6d7bfa0a3b70693f4
Tim Rayner <tim.rayner@liftshare.com>
2023-01-27 16:56:12 +0000

    add ignores

--- src/stores/table-store.ts
+++ src/stores/table-store.ts
@@ -3,10 +3,11 @@ import { defineStore } from "pinia";
 
 export const useTableStore = defineStore("table", () => {
   const tables = ref([]);
-
+  //@ts-ignore
   const addTable = (table: any) => tables.value.push(table);
-  const removeTable = (tableId: number) =>
-    (tables.value = tables.value.filter((table) => table?.id !== tableId));
+  const removeTable = (
+    tableId: number //@ts-ignore
+  ) => (tables.value = tables.value.filter((table) => table?.id !== tableId));
   const removeAllTables = () => {
     tables.value = [];
   };
